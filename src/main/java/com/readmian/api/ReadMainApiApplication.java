7def1b58abf1d80742c717a509d4ac266dfb61c1
yuehao <83541408@qq.com>
2017-08-27 10:17:38 +0800

    frist commit

---
+++ src/main/java/com/readmian/api/ReadMainApiApplication.java
@@ -0,0 +1,12 @@
+package com.readmian.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReadMainApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReadMainApiApplication.class, args);
+	}
+}
